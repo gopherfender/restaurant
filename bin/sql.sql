1fa38de72185c3e52ae211f2c7cf5c66978f6cc5
gopherfender <philgoodall83@gmail.com>
2022-03-08 14:54:30 +0000

    update with SQL testing

---
+++ bin/sql.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO items (menu_id, name, price) VALUES (7, "Porterhouse", 23.99);
+-- INSERT INTO menus (restaurant_id, name) VALUES (5, "Steaks");
+-- INSERT INTO restaurants (name, imageURL) VALUES ("Moe's Tavern", "./moes.png");
+-- SELECT * FROM items WHERE name="Bacon and Eggs";
+-- DELETE FROM menus WHERE id=8;
+-- UPDATE restaurants SET imageURL = REPLACE(imageURL, '.', './images');
+
+SELECT * FROM restaurants;
